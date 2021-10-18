edadfa9a1ff66bd01ec011a7270876990f34e3e0
Xavi-Dev <xjohndev@outlook.com>
2021-10-18 16:12:31 -0400

    Social links added/Blob added

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// MENU SHOW 
+/*------- MENU SHOW -------*/ 
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
@@ -11,9 +11,20 @@ if (navToggle) {
 
 
 
-/* ------Menu Hidden ---- */
+/* ------ Menu Hidden ------ */
 if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
+
+
+/*------Remove Menu Mobile-------*/
+const navLink = document.querySelectorAll('.nav_link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    // When each nav_link is clicked, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
