fe5d819bb3852fbaac109e1580183a4e57abafb2
Xavi-Dev <xjohndev@outlook.com>
2021-10-18 15:11:42 -0400

    Nav toggle fix

---
+++ main.js
@@ -0,0 +1,19 @@
+// MENU SHOW 
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+
+
+/* ------Menu Hidden ---- */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
